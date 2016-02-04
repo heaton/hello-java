299305242bdcca1fb8fbda4aea2da776d92ad980
Heaton Cai <heaton.cai@gmail.com>
2016-02-04 12:55:25 +0800

    more guava examples

--- src/main/java/me/heaton/guava/Person.java
+++ src/main/java/me/heaton/guava/Person.java
@@ -1,24 +1,38 @@
 package me.heaton.guava;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
+import com.google.common.collect.ComparisonChain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class Person {
+public class Person implements Comparable<Person>{
 
   final String name, nickname;
+  final int age;
   final List<String> friends;
   final Map<String, String> scores;
 
-  public Person(String name, String nickname) {
+  public Person(String name, String nickname, int age) {
     this.name = name;
     this.nickname = nickname;
+    this.age = age;
     friends = new ArrayList<>();
     scores = new HashMap<>();
+  }
+
+  public String preferredName() {
+    return MoreObjects.firstNonNull(nickname, name);
+  }
+
+  public String allFriends() {
+    return Joiner.on(", ").skipNulls().join(friends);
+  }
+
+  public String scores() {
+    return Joiner.on("; ").useForNull("NO_SCORE")
+        .withKeyValueSeparator(": ").appendTo(new StringBuilder(), scores).toString();
   }
 
   @Override public boolean equals(Object object) {
@@ -31,27 +45,24 @@ public class Person {
   }
 
   @Override public int hashCode() {
-    return Objects.hashCode(name, nickname);
+    return Objects.hashCode(name, nickname, age);
   }
 
   @Override public String toString() {
-    return Objects.toStringHelper(this)
+    return MoreObjects.toStringHelper(this)
         .add("name", name)
         .add("nickname", nickname)
+        .add("age", age)
         .toString();
   }
 
-  public String preferredName() {
-    return Objects.firstNonNull(nickname, name);
-  }
-
-  public String allFriends() {
-    return Joiner.on(", ").skipNulls().join(friends);
-  }
-
-  public String scores() {
-    return Joiner.on("; ").useForNull("NO_SCORE")
-        .withKeyValueSeparator(": ").appendTo(new StringBuilder(), scores).toString();
+  @Override
+  public int compareTo(Person that) {
+    return ComparisonChain.start()
+        .compare(this.name, that.name)
+        .compare(this.nickname, that.nickname)
+        .compare(this.age, that.age)
+        .result();
   }
 
 }
--- src/test/java/me/heaton/guava/GuavaSpec.java
+++ src/test/java/me/heaton/guava/GuavaSpec.java
@@ -1,10 +1,29 @@
 package me.heaton.guava;
 
-import com.google.common.base.Splitter;
+import com.google.common.base.*;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Ordering;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import com.google.common.math.BigIntegerMath;
+import com.google.common.math.DoubleMath;
+import com.google.common.math.IntMath;
+import com.google.common.math.LongMath;
+import com.google.common.primitives.Ints;
 import org.junit.Test;
+import org.springframework.test.context.TestExecutionListeners;
 
-import static org.hamcrest.CoreMatchers.hasItems;
-import static org.junit.Assert.assertThat;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 public class GuavaSpec {
 
@@ -12,6 +31,95 @@ public class GuavaSpec {
   public void try_splitter() {
     final Iterable<String> actual = Splitter.on(',').trimResults().omitEmptyStrings().split(" foo, ,bar, quux,");
     assertThat(actual, hasItems("foo", "bar", "quux"));
+  }
+
+  @Test
+  public void try_strings() {
+    final String s = Joiner.on(", ").skipNulls().join(ImmutableList.of("hello", "world!"));
+    assertThat(s, is("hello, world!"));
+
+    String noControl = CharMatcher.JAVA_ISO_CONTROL.removeFrom("hello"); // remove control characters
+    assertThat(noControl, is("hello"));
+
+    String theDigits = CharMatcher.DIGIT.retainFrom("abc123"); // only the digits
+    assertThat(theDigits, is("123"));
+
+    String spaced = CharMatcher.WHITESPACE.trimAndCollapseFrom("  I don't like too   much white  space!     ", ' ');
+    // trim whitespace at ends, and replace/collapse whitespace into single spaces
+    assertThat(spaced, is("I don't like too much white space!"));
+
+    String noDigits = CharMatcher.JAVA_DIGIT.replaceFrom("into 123", "*"); // star out all digits
+    assertThat(noDigits, is("into ***"));
+
+    String lowerAndDigit = CharMatcher.JAVA_DIGIT.or(CharMatcher.JAVA_LOWER_CASE).retainFrom("SOMETHING_BIG_only 123");
+    // eliminate all characters that aren't digits or lowercase
+    assertThat(lowerAndDigit, is("only123"));
+
+    final String name = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "CONSTANT_NAME");
+    assertThat(name, is("constantName"));
+  }
+
+  @Test
+  public void try_ordering() {
+    Ordering<String> byLengthOrdering = new Ordering<String>() {
+      public int compare(String left, String right) {
+        return Ints.compare(left.length(), right.length());
+      }
+    };
+    List<String> list = ImmutableList.of("I'm longest", "shorter");
+    assertTrue(byLengthOrdering.reverse().isOrdered(list));
+    assertThat(Ordering.natural().min(1, 2, 3, 4, 5), is(1));
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void try_preconditions() {
+    new Movie().setState(null);
+    fail();
+  }
+
+  @Test
+  public void try_tree() throws ExecutionException {
+    final LoadingCache<String, Integer> cache = CacheBuilder.newBuilder()
+        .maximumSize(1000)
+        .expireAfterWrite(10, TimeUnit.MINUTES)
+        .ticker(new Ticker() {
+          @Override
+          public long read() {
+            return System.currentTimeMillis();
+          }
+        })
+        .build(new CacheLoader<String, Integer>() {
+          @Override
+          public Integer load(String key) throws Exception {
+            return key.length();
+          }
+        });
+    assertThat(cache.get("abc"), is(3));
+  }
+
+  @Test
+  public void try_hashing() {
+    final HashFunction sha1 = Hashing.sha1();
+    final String hash = sha1.newHasher().putString("Heaton's password", Charsets.UTF_8).hash().toString();
+    assertThat(hash, is("10101f1c693e18833059cbf2e806f387c878f7cc"));
+  }
+
+  @Test
+  public void try_math() {
+    int logFloor = LongMath.log2(2, RoundingMode.FLOOR);
+    assertThat(logFloor, is(1));
+
+    int mustNotOverflow = IntMath.checkedMultiply(1000, 10000);
+    assertThat(mustNotOverflow, is(10000000));
+
+    long quotient = LongMath.divide(300, 3, RoundingMode.UNNECESSARY); // fail fast on non-multiple of 3
+    assertThat(quotient, is(100l));
+
+    BigInteger nearestInteger = DoubleMath.roundToBigInteger(12345.6789, RoundingMode.HALF_EVEN);
+    assertThat(nearestInteger, is(BigInteger.valueOf(12346)));
+
+    BigInteger sideLength = BigIntegerMath.sqrt(nearestInteger, RoundingMode.CEILING);
+    assertThat(sideLength, is(BigInteger.valueOf(112)));
   }
 
 }
